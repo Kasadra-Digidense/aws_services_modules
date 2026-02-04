99b8264396458094aa8beda83ea48fd68820b637
Ashwini Kanagaraj <153802512+ashwini-k-s@users.noreply.github.com>
2026-02-04 10:32:00 +0530

    dynamo

---
+++ Dev/s3-backend/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Terraform state bucket name"
+  default = "bucket-009876"
+}
+
+variable "lock_table_name" {
+  description = "DynamoDB lock table name"
+  default = "stable-lock"
+}
